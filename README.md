1f677dd95cb696495f9bec54309274dfcc1b4cf9
malkie bialestock <mbialestock@gmail.com>
2014-05-19 09:33:55 -0400

    creating new repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bialestocks
+
+This is an interesting sample application for 
+[*Bialestock family*](http://bialestockflorida.com)
+
+by [Malkie Bialestock](http://malkiebialestock.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Interesting::Application.config.secret_key_base = '7c0544c0c5c156a0cb95cb3ba29fb543fd31f01b28b3e46e8b85f5edf29c9384f37a9d5a4cefc51dfd4ba400ebd320add46a0bcb6ef1a1617475cbcc6a5512df'
